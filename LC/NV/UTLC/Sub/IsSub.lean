a65a5c809a7bb7f2013ee57b7ed78050ca71a67e
Patrick Thomas <pthomas505@gmail.com>
2024-11-13 21:32:37 -0800

    Updates.

--- LC/NV/UTLC/Sub/IsSub.lean
+++ LC/NV/UTLC/Sub/IsSub.lean
@@ -83,6 +83,144 @@ inductive is_sub : Term_ → Symbol_ → Term_ → Term_ → Prop
 -------------------------------------------------------------------------------
 
 
+-- is_sub_ M x N L means M [ x := N ] = L
+inductive is_sub_ : Term_ → Symbol_ → Term_ → Term_ → Prop
+
+-- if x = y then y [ x := N ] = N
+| var_same
+  (y : Symbol_)
+  (x : Symbol_)
+  (N : Term_) :
+  x = y →
+  is_sub_ (var_ y) x N N
+
+-- if x ≠ y then y [ x := N ] = y
+| var_diff
+  (y : Symbol_)
+  (x : Symbol_)
+  (N : Term_) :
+  ¬ x = y →
+  is_sub_ (var_ y) x N (var_ y)
+
+-- (P Q) [ x := N ] = (P [ x := N ] Q [ x := N ])
+| app
+  (P : Term_)
+  (Q : Term_)
+  (x : Symbol_)
+  (N : Term_)
+  (P' : Term_)
+  (Q' : Term_) :
+  is_sub_ P x N P' →
+  is_sub_ Q x N Q' →
+  is_sub_ (app_ P Q) x N (app_ P' Q')
+
+-- if x = y then ( λ y . P ) [ x := N ] = ( λ y . P )
+| abs_same
+  (y : Symbol_)
+  (P : Term_)
+  (x : Symbol_)
+  (N : Term_) :
+  x = y →
+  is_sub_ (abs_ y P) x N (abs_ y P)
+
+-- if x ≠ y then ( λ y . P ) [ x := N ] = ( λ y . P [ x := N ] )
+
+| abs_diff_nel
+  (y : Symbol_)
+  (P : Term_)
+  (x : Symbol_)
+  (N : Term_)
+  (P' : Term_) :
+  ¬ x = y →
+  x ∉ P.free_var_set →
+  is_sub_ P x N P' →
+  is_sub_ (abs_ y P) x N (abs_ y P')
+
+| abs_diff
+  (y : Symbol_)
+  (P : Term_)
+  (x : Symbol_)
+  (N : Term_)
+  (P' : Term_) :
+  ¬ x = y →
+  y ∉ N.free_var_set →
+  is_sub_ P x N P' →
+  is_sub_ (abs_ y P) x N (abs_ y P')
+
+
+example
+  (e1 e2 e3 : Term_)
+  (v : Symbol_)
+  (h1 : is_sub e1 v e2 e3) :
+  is_sub_ e1 v e2 e3 :=
+  by
+    induction h1
+    case var_same y x N ih =>
+      apply is_sub_.var_same
+      exact ih
+    case var_diff y x N ih =>
+      apply is_sub_.var_diff
+      exact ih
+    case app P Q x N P' Q' ih_1 ih_2 ih_3 ih_4 =>
+      apply is_sub_.app
+      · exact ih_3
+      · exact ih_4
+    case abs_same y P x N ih_1 =>
+      apply is_sub_.abs_same
+      · exact ih_1
+    case abs_diff_nel y P x N P' ih_1 ih_2 ih_3 ih_4 =>
+      unfold free_var_set at ih_2
+      simp at ih_2
+      have s1 : x ∉ P.free_var_set := by tauto
+      apply is_sub_.abs_diff_nel
+      · exact ih_1
+      · exact s1
+      · exact ih_4
+    case abs_diff y P x N P' ih_1 ih_2 ih_3 ih_4 =>
+      apply is_sub_.abs_diff
+      · exact ih_1
+      · exact ih_2
+      · exact ih_4
+
+
+example
+  (e1 e2 e3 : Term_)
+  (v : Symbol_)
+  (h1 : is_sub_ e1 v e2 e3) :
+  is_sub e1 v e2 e3 :=
+  by
+    induction h1
+    case var_same y x N ih =>
+      apply is_sub.var_same
+      exact ih
+    case var_diff y x N ih =>
+      apply is_sub.var_diff
+      exact ih
+    case app P Q x N P' Q' ih_1 ih_2 ih_3 ih_4 =>
+      apply is_sub.app
+      · exact ih_3
+      · exact ih_4
+    case abs_same y P x N ih_1 =>
+      apply is_sub.abs_same
+      · exact ih_1
+    case abs_diff_nel y P x N P' ih_1 ih_2 ih_3 ih_4 =>
+      apply is_sub.abs_diff_nel
+      · exact ih_1
+      · unfold free_var_set
+        simp
+        intro a1
+        contradiction
+      · exact ih_4
+    case abs_diff y P x N P' ih_1 ih_2 ih_3 ih_4 =>
+      apply is_sub.abs_diff
+      · exact ih_1
+      · exact ih_2
+      · exact ih_4
+
+
+-------------------------------------------------------------------------------
+
+
 -- https://faculty.iiit.ac.in/~venkatesh.choppella/popl/current-topics/lambda-calculus-1/index.html#org6f14feb
 
 -- is_sub_fresh M x N L means M [ x := N ] = L
