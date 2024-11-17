7e765d3f5dd36fb0285cec969c3237d3dc4e9b22
Patrick Thomas <pthomas505@gmail.com>
2024-11-17 12:16:49 -0800

    Updates.

--- LC/NV/UTLC/Sub/SubIsDef.lean
+++ LC/NV/UTLC/Sub/SubIsDef.lean
@@ -61,28 +61,28 @@ inductive sub_is_def_v3 : Term_ → Symbol_ → Term_ → Prop
 
 -- [1]
 lemma lemma_1_2_5_i_a
-  (e1 : Term_)
-  (v : Symbol_)
-  (e2 : Term_)
-  (h1 : v ∉ e1.free_var_set) :
-  sub_is_def_v3 e1 v e2 :=
+  (M : Term_)
+  (x : Symbol_)
+  (N : Term_)
+  (h1 : x ∉ M.free_var_set) :
+  sub_is_def_v3 M x N :=
   by
-  induction e1
-  case var_ x =>
-    exact sub_is_def_v3.var x v e2
+  induction M
+  case var_ x_ =>
+    exact sub_is_def_v3.var x_ x N
 
-  case app_ P Q ih_1 ih_2 =>
+  case app_ P_ Q_ ih_1 ih_2 =>
     unfold Term_.free_var_set at h1
     simp at h1
     obtain ⟨h1_left, h1_right⟩ := h1
     specialize ih_1 h1_left
     specialize ih_2 h1_right
-    exact sub_is_def_v3.app P Q v e2 ih_1 ih_2
+    exact sub_is_def_v3.app P_ Q_ x N ih_1 ih_2
 
-  case abs_ x P _ =>
-    by_cases c1 : v = x
-    · exact sub_is_def_v3.abs_1 x P v e2 c1
-    · apply sub_is_def_v3.abs_2 x P v e2 c1
+  case abs_ x_ P_ _ =>
+    by_cases c1 : x = x_
+    · exact sub_is_def_v3.abs_1 x_ P_ x N c1
+    · apply sub_is_def_v3.abs_2 x_ P_ x N c1
       unfold free_var_set at h1
       simp at h1
       tauto
@@ -90,16 +90,16 @@ lemma lemma_1_2_5_i_a
 
 -- [1]
 lemma lemma_1_2_5_ii_right
-  (e1 : Term_)
-  (v : Symbol_)
-  (e2 : Term_)
-  (z : Symbol_)
-  (h1 : sub_is_def_v3 e1 v e2)
-  (h2 : z ∈ (replace_free v e2 e1).free_var_set) :
-  (z ∈ e1.free_var_set ∧ ¬ v = z) ∨ (z ∈ e2.free_var_set ∧ v ∈ e1.free_var_set) :=
+  (M : Term_)
+  (x : Symbol_)
+  (N : Term_)
+  (y : Symbol_)
+  (h1 : sub_is_def_v3 M x N)
+  (h2 : y ∈ (replace_free x N M).free_var_set) :
+  (y ∈ M.free_var_set ∧ ¬ x = y) ∨ (y ∈ N.free_var_set ∧ x ∈ M.free_var_set) :=
   by
     induction h1
-    case var y x N =>
+    case var y_ x_ N_ =>
       simp only [free_var_set]
       simp
 
@@ -114,7 +114,7 @@ lemma lemma_1_2_5_ii_right
         left
         rw [h2]
         exact ⟨rfl, c1⟩
-    case app P Q x N ih_1 ih_2 ih_3 ih_4 =>
+    case app P_ Q_ x_ N_ ih_1 ih_2 ih_3 ih_4 =>
       simp only [free_var_set]
       simp
 
@@ -126,7 +126,7 @@ lemma lemma_1_2_5_ii_right
         tauto
       case inr h2_right =>
         tauto
-    case abs_1 y P x N ih =>
+    case abs_1 y_ P_ x_ N_ ih =>
       unfold replace_free at h2
       split_ifs at h2
       unfold free_var_set at h2
@@ -137,7 +137,7 @@ lemma lemma_1_2_5_ii_right
       simp
       rw [ih]
       tauto
-    case abs_2 y P x N ih_1 ih_2 =>
+    case abs_2 y_ P_ x_ N_ ih_1 ih_2 =>
       unfold replace_free at h2
       split_ifs at h2
       unfold free_var_set at h2
@@ -147,7 +147,7 @@ lemma lemma_1_2_5_ii_right
       simp only [free_var_set]
       simp
 
-      have s1 : (replace_free x N P) = P := replace_free_not_mem x N P ih_2
+      have s1 : (replace_free x_ N_ P_) = P_ := replace_free_not_mem x_ N_ P_ ih_2
       rw [s1] at h2
 
       constructor
@@ -155,7 +155,7 @@ lemma lemma_1_2_5_ii_right
       · intro contra
         rw [← contra] at h2
         tauto
-    case abs_3 y P x N ih_1 ih_2 ih_3 ih_4 =>
+    case abs_3 y_ P_ x_ N_ ih_1 ih_2 ih_3 ih_4 =>
       unfold replace_free at h2
       split_ifs at h2
       unfold free_var_set at h2
@@ -168,20 +168,20 @@ lemma lemma_1_2_5_ii_right
 
 -- [1]
 lemma lemma_1_2_5_ii_left
-  (e1 : Term_)
-  (v : Symbol_)
-  (e2 : Term_)
-  (z : Symbol_)
-  (h1 : sub_is_def_v3 e1 v e2)
-  (h2 : (z ∈ e1.free_var_set ∧ ¬ v = z) ∨ (z ∈ e2.free_var_set ∧ v ∈ e1.free_var_set)) :
-  z ∈ (replace_free v e2 e1).free_var_set :=
+  (M : Term_)
+  (x : Symbol_)
+  (N : Term_)
+  (y : Symbol_)
+  (h1 : sub_is_def_v3 M x N)
+  (h2 : (y ∈ M.free_var_set ∧ ¬ x = y) ∨ (y ∈ N.free_var_set ∧ x ∈ M.free_var_set)) :
+  y ∈ (replace_free x N M).free_var_set :=
   by
     induction h1
     all_goals
       simp only [free_var_set] at h2
 
       unfold replace_free
-    case var y x N =>
+    case var y_ x_ N_ =>
       simp at h2
 
       split_ifs
@@ -192,31 +192,31 @@ lemma lemma_1_2_5_ii_left
         unfold free_var_set
         simp
         tauto
-    case app P Q x N ih_1 ih_2 ih_3 ih_4 =>
+    case app P_ Q_ x_ N_ ih_1 ih_2 ih_3 ih_4 =>
       simp at h2
 
       unfold free_var_set
       simp
       tauto
-    case abs_1 y P x N ih =>
+    case abs_1 y_ P_ x_ N_ ih =>
       simp at h2
 
       split_ifs
       unfold free_var_set
       simp
       tauto
-    case abs_2 y P x N ih_1 ih_2 =>
+    case abs_2 y_ P_ x_ N_ ih_1 ih_2 =>
       simp at h2
 
       split_ifs
       unfold free_var_set
       simp
 
-      have s1 : replace_free x N P = P := replace_free_not_mem x N P ih_2
+      have s1 : replace_free x_ N_ P_ = P_ := replace_free_not_mem x_ N_ P_ ih_2
       rw [s1]
 
       tauto
-    case abs_3 y P x N ih_1 ih_2 ih_3 ih_4 =>
+    case abs_3 y_ P_ x_ N_ ih_1 ih_2 ih_3 ih_4 =>
       simp at h2
 
       split_ifs
@@ -238,16 +238,16 @@ lemma lemma_1_2_5_ii_left
 
 -- [1]
 lemma lemma_1_2_5_ii
-  (e1 : Term_)
-  (v : Symbol_)
-  (e2 : Term_)
-  (z : Symbol_)
-  (h1 : sub_is_def_v3 e1 v e2) :
-  z ∈ (replace_free v e2 e1).free_var_set ↔
-    (z ∈ e1.free_var_set ∧ ¬ v = z) ∨ (z ∈ e2.free_var_set ∧ v ∈ e1.free_var_set) :=
+  (M : Term_)
+  (x : Symbol_)
+  (N : Term_)
+  (y : Symbol_)
+  (h1 : sub_is_def_v3 M x N) :
+  y ∈ (replace_free x N M).free_var_set ↔
+    (y ∈ M.free_var_set ∧ ¬ x = y) ∨ (y ∈ N.free_var_set ∧ x ∈ M.free_var_set) :=
   by
     induction h1
-    case var y x N =>
+    case var y_ x_ N_ =>
       unfold replace_free
       split_ifs
       case pos c1 =>
@@ -264,32 +264,32 @@ lemma lemma_1_2_5_ii
           tauto
         · intro a1
           tauto
-    case app P Q x N ih_1 ih_2 ih_3 ih_4 =>
+    case app P_ Q_ x_ N_ ih_1 ih_2 ih_3 ih_4 =>
       unfold replace_free
       simp only [free_var_set]
       simp
       tauto
-    case abs_1 y P x N ih =>
+    case abs_1 y_ P_ x_ N_ ih =>
       unfold replace_free
       split_ifs
       simp only [free_var_set]
       simp
       rw [ih]
       tauto
-    case abs_2 y P x N ih_1 ih_2 =>
+    case abs_2 y_ P_ x_ N_ ih_1 ih_2 =>
       unfold replace_free
       split_ifs
       simp only [free_var_set]
       simp
 
-      have s1 : replace_free x N P = P := replace_free_not_mem x N P ih_2
+      have s1 : replace_free x_ N_ P_ = P_ := replace_free_not_mem x_ N_ P_ ih_2
       rw [s1]
 
       constructor
       · intro a1
         obtain ⟨a1_left, a1_right⟩ := a1
 
-        have s1 : ¬ x = z :=
+        have s1 : ¬ x_ = y :=
         by
           intro contra
           apply ih_2
@@ -298,7 +298,7 @@ lemma lemma_1_2_5_ii
 
         tauto
       · tauto
-    case abs_3 y P x N ih_1 ih_2 ih_3 ih_4 =>
+    case abs_3 y_ P_ x_ N_ ih_1 ih_2 ih_3 ih_4 =>
       unfold replace_free
       split_ifs
       simp only [free_var_set]
@@ -323,19 +323,19 @@ lemma lemma_1_2_5_ii
 
 -- [1]
 lemma lemma_1_2_5_iii_a
-  (e : Term_)
-  (v : Symbol_) :
-  sub_is_def_v3 e v (var_ v) :=
+  (M : Term_)
+  (x : Symbol_) :
+  sub_is_def_v3 M x (var_ x) :=
   by
-    induction e
-    case var_ x =>
+    induction M
+    case var_ x_ =>
       apply sub_is_def_v3.var
-    case app_ P Q ih_1 ih_2 =>
+    case app_ P_ Q_ ih_1 ih_2 =>
       apply sub_is_def_v3.app
       · exact ih_1
       · exact ih_2
-    case abs_ x P ih =>
-      by_cases c1 : v = x
+    case abs_ x_ P_ ih =>
+      by_cases c1 : x = x_
       case pos =>
         rw [c1]
         apply sub_is_def_v3.abs_1
